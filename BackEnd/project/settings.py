ce7576c53c0f728b920ec5d74f83609579e9440c
Mohamed-Silaya <mohamedsamer1532@gmail.com>
2025-08-08 18:59:04 +0300

    adding api function (summarize_view)

--- BackEnd/project/settings.py
+++ BackEnd/project/settings.py
@@ -37,9 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #########
+    'summarize',
+    'corsheaders',
+
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -47,8 +53,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
+CORS_ALLOW_ALL_ORIGINS = True
+# ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['http://localhost:5173']
 ROOT_URLCONF = 'project.urls'
 
 TEMPLATES = [
--- BackEnd/summarize/views.py
+++ BackEnd/summarize/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import requests
+import os
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+from  dotenv import load_dotenv
+load_dotenv()
+
+
+@csrf_exempt
+def summarize_view(request):
+    if request.method =="POST":
+        import json
+        body = json.loads(request.body.decode())
+        text = body.get("inputs", "")
+        HF_TOKEN = os.environ.get("HF_TOKEN")
+        api_url = "https://router.huggingface.co/hf-inference/models/facebook/bart-large-cnn"
+        resp = requests.post(api_url,
+            headers={
+                "Authorization": f"Bearer {HF_TOKEN}",
+                "Content-Type": "application/json"
+            },
+            json={"inputs": text}
+
+        )
+        print("HF Response:", resp.text)
+        return JsonResponse(resp.json(), safe=False)
+    return JsonResponse({"error": "Method not allowed"}, status=405)
