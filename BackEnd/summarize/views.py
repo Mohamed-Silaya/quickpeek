7e3707e4d6657481230af0011ae35a35d595a2c9
Mohamed-Silaya <mohamedsamer1532@gmail.com>
2025-08-08 19:20:26 +0300

    enhance api response

--- BackEnd/summarize/views.py
+++ BackEnd/summarize/views.py
@@ -15,13 +15,22 @@ def summarize_view(request):
         body = json.loads(request.body.decode())
         text = body.get("inputs", "")
         HF_TOKEN = os.environ.get("HF_TOKEN")
-        api_url = "https://router.huggingface.co/hf-inference/models/facebook/bart-large-cnn"
+        # api_url = "https://router.huggingface.co/hf-inference/models/facebook/bart-large-cnn"
+        api_url = "https://api-inference.huggingface.co/models/facebook/bart-large-cnn"
+
         resp = requests.post(api_url,
             headers={
                 "Authorization": f"Bearer {HF_TOKEN}",
                 "Content-Type": "application/json"
             },
-            json={"inputs": text}
+            # json={"inputs": text }
+            json={
+            "inputs": text,
+            "parameters": {
+                "max_length": 60,   
+                "min_length": 15
+            }}
+
 
         )
         print("HF Response:", resp.text)
